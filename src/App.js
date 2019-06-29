2d2ee9712344dd905c2e9282c7d02a2a1c7b878d
Fred Collman <fredcollman@gmail.com>
2019-06-29 18:06:15 +0100

    Add basic title slide

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import React from "react";
 import { hot } from "react-hot-loader/root";
 import "./App.css";
 
-const App = () => <div>yes</div>;
+const TitleSlide = ({ title, subtitle }) => (
+  <article className="width-m height-m bg-subtle">
+    <div className="cover height-full">
+      <div className="center">
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+      </div>
+    </div>
+  </article>
+);
+
+const App = () => (
+  <main>
+    <TitleSlide title="Title Slide" subtitle="by Sub Title" />
+  </main>
+);
 
 export default hot(App);
